b93929f08fc4680b6a75aeeff1fc57b3ca2f1cdb
Elliott <elliott@myconnect.com.au>
2023-05-06 19:57:11 +1000

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace weather_app_dotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
